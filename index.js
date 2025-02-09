921755d77cea536e25854f54d56395242e2bffd0
SaiyedMustaq <150017658+MustaqAhmed501@users.noreply.github.com>
2025-02-09 21:01:13 +0530

    - sing in and signup page add - user route add

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-path = require('path');
-const app = express()
-const port = 3000
+const express = require("express");
+const mongoose = require("mongoose");
+path = require("path");
+const app = express();
+const port = 3000;
+const userRouter = require("./routes/user");
 
-app.set("view engine", 'ejs');
+mongoose.connect("mongodb://127.0.0.1:27017/blog", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use("/user", userRouter);
+
+app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get("/", (req, res) => res.render("home"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ models/user.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require("mongoose");
+const { createHmac, randomBytes } = require("crypto");
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Please enter a valid email address"],
+    },
+    password: {
+      salt: {
+        type: String,
+        required: true,
+      },
+      type: String,
+      required: true,
+    },
+    profileImageUrl: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+  const salt = randomBytes(16).toString();
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+  this.salt = salt;
+  this.password = hashedPassword;
+  next();
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
